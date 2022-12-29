318d5b4cbe727518c71790b337be4109a08ddc65
bderya <94176553+bderya@users.noreply.github.com>
2022-12-29 20:41:35 +0100

    Update ft_strjoin.c
    Strjoin is not working :))

--- libft/ft_strjoin.c
+++ libft/ft_strjoin.c
@@ -25,7 +25,7 @@ char	*ft_strjoin(char *s1, const char *s2)
 	i = 0;
 	i2 = 0;
 	c = malloc(ft_strlen(s1) + ft_strlen(s2) + 1);
-	if (c == '\0')
+	if (c == (void *) 0)
 		return (0);
 	while (s1 && s1[i] != '\0')
 	{
